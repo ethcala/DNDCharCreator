734cb587332a8288951f4980b1e6f7f3587b8414
madmanham11 <smithphilip812@gmail.com>
2023-05-22 15:04:29 -0600

    created add character baseline at least with boxes and added a script might want to change it to store in the database

--- DNDCreator/characters/AddCharacter.php
+++ DNDCreator/characters/AddCharacter.php
@@ -1,3 +1,69 @@
 <?php
 
 ?>
+<br />
+<table>
+    <tr>
+        <td>
+            <input type="text" name="Hero Name" />
+        </td>
+        <td>
+            <input type="text" name="Gender" />
+        </td>
+        <td>
+            <input type="text" name="Class" />
+        </td>
+        <td>
+            <input type="text" name="Race" />
+        </td>
+        <td>
+            <input type="text" name="Flaws" />
+        </td>
+        <td>
+            <input type="text" name="Level" />
+        </td>
+        <td>
+            <input type="text" name="HP" />
+        </td>
+        <td>
+            <input type="text" name="AC" />
+        </td>
+        <td>
+            <input type="text" name="strength" />
+        </td>
+        <td>
+            <input type="text" name="dexterity" />
+        </td>
+        <td>
+            <input type="text" name="charisma" />
+        </td>
+        <td>
+            <input type="text" name="constitution" />
+        </td>
+        <td>
+            <input type="text" name="wisdom" />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <button name="create" onclick="addElement()">Create Character</button>
+        </td>
+    </tr>
+</table>
+<script>
+    function setelem(tag) {
+        document.getElementById("sel").value = tag;
+    }
+
+    function addElement() {
+        let selection = document.getElementById("sel").value;
+        let content = document.getElementById("content").value;
+        if (selection == null || selection == '' || content == null || content == '') {
+            document.getElementById('error').innerHTML = "Could not create new element, fill in all fields.";
+        }
+
+        let element = "<" + selection + ">" + content + "</" + selection + ">";
+        document.getElementById('preview').innerHTML += element;
+    }
+
+</script>
