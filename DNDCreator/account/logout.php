da2ca4fc8fcc6d27afe249e84424e9b4de0155c1
madmanham11 <smithphilip812@gmail.com>
2023-05-26 23:56:26 -0600

    fixed logout but might need more work

--- DNDCreator/account/logout.php
+++ DNDCreator/account/logout.php
@@ -1,3 +1,8 @@
 <?php
-    
+    if (isset($_COOKIE["user"])) {
+    setcookie("user", "", time() - 100000, "/");
+    setcookie("password", "", time() - 100000, "/");
+    header("Location: http://localhost:5435/");
+    exit();
+}
 ?>
